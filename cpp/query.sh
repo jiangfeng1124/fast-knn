3dc6cf75a1195404788a24895d8d75ebb315cb3b
jiangfeng1124 <jiangfeng1124@gmail.com>
2013-10-16 10:28:21 +0800

    fixed bin_path bug

--- cpp/query.sh
+++ cpp/query.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+root=`pwd`
 data=example.dat
-bin=/home/jiangfeng/Work/fast-knn/cpp/ann_1.1.2/bin
+bin=$root/cpp/ann_1.1.2/bin
 
 $bin/ann_sample -d 25 -max 5000 -nn 10 -df $data -qf $data > cpp.sim
 
